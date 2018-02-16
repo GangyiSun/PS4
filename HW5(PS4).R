704fbb2d16a8e5b517ab0630e8a25cb0c6ae8921
Gangyi Sun <gangyi.sun@wustl.edu>
2018-02-16 15:30:51 -0600

    Identified another problem with myFunction.

--- HW5(PS4).R
+++ HW5(PS4).R
@@ -6,7 +6,8 @@
 # Part 1: Getting started 
 # The following code needs to be debugged and improved
 myFunction<-function(doorthing, doorthing2, x){
-  doorthing1<-doorthing2<-sample(1:3, 1)
+  doorthing1<-sample(1:3, 1)
+  doorthing2<-sample(1:3, 1)
   if (doorthing1==doorthing2){ x<-TRUE } else { x==FALSE }
   x
 }
@@ -21,3 +22,6 @@ undebug(myFunction)
 # By running debug multiple times, we see that our initial hypothesis of the problem is correct. Every time we run 
 # debug, doorthing1 and doorthing2 always takes the same value. This can be fixed by assigning sample to doorthing1
 # and doorthing2 seperately. 
+# Another problem is identified after fixing the first problem of how values to doorthing1 and doorthing2 are assigned.
+# If doorthing1 and doorthing2 are not equal, an error is thrown. This is becuase in line 11, == is used wrongly. 
+# An assignment operator should be used to assign FALSE to x, instead of a logical equality sign. 
