6fd8706723c9f81761ce600fbdb6f9e4b6f29062
Gangyi Sun <gangyi.sun@wustl.edu>
2018-02-16 15:32:37 -0600

    debugged myFunction

--- HW5(PS4).R
+++ HW5(PS4).R
@@ -8,12 +8,10 @@
 myFunction<-function(doorthing, doorthing2, x){
   doorthing1<-sample(1:3, 1)
   doorthing2<-sample(1:3, 1)
-  if (doorthing1==doorthing2){ x<-TRUE } else { x==FALSE }
+  if (doorthing1==doorthing2){ x<-TRUE } else { x<-FALSE }
   x
 }
-debug(myFunction)
 myFunction(sample(1:3, 1), sample(1:3, 1))
-undebug(myFunction)
 # Should return a TRUE if these samples are equal and
 # a false if they are not
 # It appears that the function is always returning TRUE. 
@@ -25,3 +23,4 @@ undebug(myFunction)
 # Another problem is identified after fixing the first problem of how values to doorthing1 and doorthing2 are assigned.
 # If doorthing1 and doorthing2 are not equal, an error is thrown. This is becuase in line 11, == is used wrongly. 
 # An assignment operator should be used to assign FALSE to x, instead of a logical equality sign. 
+# After these changes, myFunction is now fixed. It returns TRUE sometimes and FALSE sometimes. 
