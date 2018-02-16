eb86b18105107608598758ae81214b2e791f4991
Gangyi Sun <gangyi.sun@wustl.edu>
2018-02-16 15:09:07 -0600

    Added thoughts on potential problems before debug.

--- HW5(PS4).R
+++ HW5(PS4).R
@@ -13,3 +13,6 @@ myFunction<-function(doorthing, doorthing2, x){
 myFunction(sample(1:3, 1), sample(1:3, 1))
 # Should return a TRUE if these samples are equal and
 # a false if they are not
+# It appears that the function is always returning TRUE. 
+# It seems like the assignment in line 9 causes doorthing1 and doorthing2 to have the same integer value. 
+# use debug() to check this and debug 
