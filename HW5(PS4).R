797aa44fbfb8987b9bee171c6bca44f550cbfdc4
Gangyi Sun <gangyi.sun@wustl.edu>
2018-02-16 15:26:33 -0600

    Used debug to identify problem with myFunction.

--- HW5(PS4).R
+++ HW5(PS4).R
@@ -10,9 +10,14 @@ myFunction<-function(doorthing, doorthing2, x){
   if (doorthing1==doorthing2){ x<-TRUE } else { x==FALSE }
   x
 }
+debug(myFunction)
 myFunction(sample(1:3, 1), sample(1:3, 1))
+undebug(myFunction)
 # Should return a TRUE if these samples are equal and
 # a false if they are not
 # It appears that the function is always returning TRUE. 
 # It seems like the assignment in line 9 causes doorthing1 and doorthing2 to have the same integer value. 
 # use debug() to check this and debug 
+# By running debug multiple times, we see that our initial hypothesis of the problem is correct. Every time we run 
+# debug, doorthing1 and doorthing2 always takes the same value. This can be fixed by assigning sample to doorthing1
+# and doorthing2 seperately. 
